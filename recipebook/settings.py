db87136ad0ba8f5d3ff1a47c9bc77ed482dd7374
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-02-27 22:38:00 +0800

    fixed os import getenv

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from os import os
+from os import getenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
