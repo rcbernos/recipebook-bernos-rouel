d7bb8a2e7e7a7994734e53dec738cc3d581c9379
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-02-28 09:25:53 +0800

    fixed urls in ledger app

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, list
+
 urlpatterns = [
     path('', index, name='index'),
-    path('/recipes/list', index, name='list'),
+    path('recipes/list', list, name='list'),
 ]
 app_name = "ledger"
