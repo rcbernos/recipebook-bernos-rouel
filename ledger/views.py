4917973f224e7631bcaf4304bd493db6d05be237
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-01 14:54:24 +0800

    modified list function to return base template

--- ledger/views.py
+++ ledger/views.py
@@ -7,4 +7,13 @@ def index(request):
     return HttpResponse('Hello World! This came from the index view')
 
 def list(request):
-    return HttpResponse('This is from /recipes/list')
+    # return HttpResponse('This is from /recipes/list')
+    ctx = {
+        "tasks": [
+            "task 1",
+            "task 2",
+            "task 3",
+            "task 4"
+        ]
+    }
+    return render(request, 'templates/base.html',ctx)
