e4e65d3938b1468aa3b3135d7ba6265f84bcf9f7
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-03 19:48:14 +0800

    added import for recipe1 and commented out url for recipe2

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import index, list
+from .views import index, list, recipe1
 
 urlpatterns = [
     path('', index, name='index'),
     path('recipes/list', list, name='list'),
     path('recipe/1', recipe1, name='recipe1'),
-    path('recipe/2', recipe2, name='recipe2'),
+    # path('recipe/2', recipe2, name='recipe2'),
 ]
 app_name = "ledger"
