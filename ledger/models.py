2de851997d3426b9e906f8cb029338bc934e3cb1
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-07 01:49:28 +0800

    changed max_length for all models to 101

--- ledger/models.py
+++ ledger/models.py
@@ -4,20 +4,20 @@ from django.urls import reverse
 # Create your models here.
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=101)
     def __str__(self):
         return '{}'.format(self.name)
     def get_absolute_url(self):
         return reverse('ingredient_detail', args=[str(self.name)])
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=101)
     def __str__(self):
         return '{}'.format(self.name)
     def get_absolute_url(self):
         return reverse('recipe_detail', args=[str(self.name)])
 
 class RecipeIngredient(models.Model):
-    quantity = models.CharField(max_length=100)
+    quantity = models.CharField(max_length=101)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
