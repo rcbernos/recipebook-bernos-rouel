7e8346f782933e20064fc11b9525af81bc0c1685
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-06 08:49:04 +0800

    removed name and due_Date and added quantity, ingredient, and recipe fields

--- ledger/models.py
+++ ledger/models.py
@@ -9,5 +9,6 @@ class Recipe(models.Model):
     name = models.CharField(max_length=100)
 
 class RecipeIngredient(models.Model):
-    name = models.CharField(max_length=100)
-    due_date = models.DateTimeField(null=False)
+    quantity = models.PositiveIntegerField(null=False)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
