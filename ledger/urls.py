1b2a89a5bd9711536086a0f628e17790c3162486
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-07 01:20:27 +0800

    reverted to previous paths

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,6 +3,7 @@ from .views import index, list, recipe1, recipe2
 
 urlpatterns = [
     path('recipes/list', list, name='list'),
-    path('recipe/<int:pk>', recipe1, name='recipe<int:pk>'),
+    path('recipe/1', recipe1, name='recipe1'),
+    path('recipe/2', recipe2, name='recipe2'),
 ]
 app_name = "ledger"
