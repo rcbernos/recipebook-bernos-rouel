0e27517e8a24cc63d0a76d0901c9922093dbdebf
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-07 01:08:28 +0800

    added max length to RecipeIngredient quantity

--- ledger/models.py
+++ ledger/models.py
@@ -15,9 +15,9 @@ class Recipe(models.Model):
     def __str__(self):
         return '{}'.format(self.name)
     def get_absolute_url(self):
-        return reverse('ingredient_detail', args=[str(self.name)])
+        return reverse('recipe_detail', args=[str(self.name)])
 
 class RecipeIngredient(models.Model):
-    quantity = models.PositiveIntegerField(null=False)
+    quantity = models.PositiveIntegerField(max_length=100,null=False)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
