7279a6670d8c6a7d809e219d4376f040c1f6289c
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-06 08:59:40 +0800

    removed index url path

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import index, list, recipe1, recipe2
 
 urlpatterns = [
-    path('', index, name='index'),
     path('recipes/list', list, name='list'),
     path('recipe/1', recipe1, name='recipe1'),
     path('recipe/2', recipe2, name='recipe2'),
