90af7c28b1eeeeef81419673fbf2768753375727
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-06 08:41:28 +0800

    removed due_date field from Ingredient

--- ledger/models.py
+++ ledger/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
-    due_date = models.DateTimeField(null=False)
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
