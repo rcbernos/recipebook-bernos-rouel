594e93907c2819893de3948d27637cc901f29539
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-06 13:58:21 +0800

    added related names to ingredient and recipe

--- ledger/models.py
+++ ledger/models.py
@@ -19,5 +19,5 @@ class Recipe(models.Model):
 
 class RecipeIngredient(models.Model):
     quantity = models.PositiveIntegerField(null=False)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
