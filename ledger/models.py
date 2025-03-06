be13c9439e1a316988ee364bc73e0608450a70de
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-06 08:53:56 +0800

    added string and absolute url functions to ingredient

--- ledger/models.py
+++ ledger/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return '{}'.format(self.name)
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.name)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
