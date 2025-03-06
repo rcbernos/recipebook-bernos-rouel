295eed67a36c648a9b0432214b2f876e942c9a11
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-07 01:14:24 +0800

    merged to paths into one

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,6 @@ from .views import index, list, recipe1, recipe2
 
 urlpatterns = [
     path('recipes/list', list, name='list'),
-    path('recipe/1', recipe1, name='recipe1'),
-    path('recipe/2', recipe2, name='recipe2'),
+    path('recipe/<int:pk>', recipe1, name='recipe<int:pk>'),
 ]
 app_name = "ledger"
