8d6ed657aa27ebf3b142b238526717cf5b5c9824
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-08 09:41:24 +0800

    Edited code to meet pep8 standards

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from .models import Ingredient, Recipe, RecipeIngredient
-#test
+
+
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
+
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
 
+
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
+
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
--- ledger/models.py
+++ ledger/models.py
@@ -1,23 +1,30 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=101)
+
     def __str__(self):
         return self.name
+
     def get_absolute_url(self):
         return reverse('ledger:ingredient-detail', args=[str(self.pk)])
 
+
 class Recipe(models.Model):
     name = models.CharField(max_length=101)
+
     def __str__(self):
         return self.name
+
     def get_absolute_url(self):
         return reverse('ledger:recipe-detail', args=[str(self.pk)])
 
+
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=101)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='ingredients')
