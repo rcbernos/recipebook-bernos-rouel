5f24ffc936a8fddb695344ad867e4cc7f8bbf3e9
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-11 00:43:58 +0800

    edited to match pep8 standard

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     name = models.OneToOneField(User, max_length=50, on_delete=models.CASCADE)
-    short_bio = models.TextField(validators=[MinLengthValidator(50, 'the field must contain at least 50 characters')])
+    short_bio = models.TextField(validators=[MinLengthValidator(
+        50, 'the field must contain at least 50 characters')])
