e1f72e03c60d4385331de039d561e8fd2d716cfd
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-11 00:31:34 +0800

    removed access to detail view from logged out users

--- ledger/views.py
+++ ledger/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class RecipeListView(ListView):
@@ -9,6 +10,7 @@ class RecipeListView(ListView):
     template_name = "recipe_list.html"
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = "recipe_detail.html"
+    redirect_field_name = 'recipes/list'
