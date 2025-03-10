c50f9ffc13c957074119ac296c80c6e455367eed
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-10 21:45:59 +0800

    renamed user field to name and removed old name field under Profile

--- ledger/models.py
+++ ledger/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinLengthValidator
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, max_length=50, on_delete=models.CASCADE)
+    name = models.OneToOneField(User, max_length=50, on_delete=models.CASCADE)
     short_bio = models.TextField(validators=[MinLengthValidator(50, 'the field must contain at least 50 characters')])
 
 
