32bcb2f096c52b2926c65af952f516768780d9a2
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-10 23:29:24 +0800

    registered admin for Profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin():
+    model = Profile
+
+
+admin.site.register(Profile, ProfileAdmin)
