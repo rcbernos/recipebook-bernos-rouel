05b97d9f4e78ef4d4680ec15520118bbeb0a0e4d
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-10 23:29:03 +0800

    moved Profile model from ledger app to accounts app

--- ledger/models.py
+++ ledger/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from django.core.validators import MinLengthValidator
-from django.contrib.auth.models import User
-
-class Profile(models.Model):
-    name = models.OneToOneField(User, max_length=50, on_delete=models.CASCADE)
-    short_bio = models.TextField(validators=[MinLengthValidator(50, 'the field must contain at least 50 characters')])
 
 
 class Ingredient(models.Model):
