fdca549545c4ee115eccb099ac4a9ccea3485632
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-10 18:05:55 +0800

    added creation and update date

--- ledger/models.py
+++ ledger/models.py
@@ -21,6 +21,8 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     name = models.CharField(max_length=101)
     author = models.models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
