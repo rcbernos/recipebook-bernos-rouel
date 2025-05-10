5dbc9bf50b43b44338767980f82d6b91b0ab30cf
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-05-10 11:39:50 +0800

    fixed typo in RecipeImage

--- ledger/models.py
+++ ledger/models.py
@@ -36,4 +36,4 @@ class RecipeImage(models.Model):
     image = models.ImageField((""), upload_to='images/', null=False)
     description = models.CharField(max_length=255)
     recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name='recipeimage')
+        Recipe, on_delete=models.CASCADE, related_name='ingredients')
