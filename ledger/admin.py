e207390ff4aed97ef6269943bee5cdeedfd5dbf0
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-05-10 12:04:26 +0800

    pep8 format

--- ledger/admin.py
+++ ledger/admin.py
@@ -5,8 +5,10 @@ from .models import Ingredient, Recipe, RecipeIngredient, RecipeImage
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
+
 class RecipeImageInline(admin.TabularInline):
     model = RecipeImage
+
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
