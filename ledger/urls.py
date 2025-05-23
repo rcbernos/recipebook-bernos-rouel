abd41f49d73cd1fea1c6775c9f5c9893110dcbf6
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-05-23 13:20:46 +0800

    updated recipe add url

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,7 +4,7 @@ from .views import RecipeListView, RecipeDetailView, RecipeAddView, ImageAddView
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipe-list'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail'),
-    path('recipe/add', RecipeAddView.as_view(), name='recipe-add'),
+    path('recipe/add', RecipeAddView, name='recipe-add'),
     path('recipe/<int:pk>/add_image', ImageAddView.as_view(), name='image-add'),
 ]
 app_name = "ledger"
